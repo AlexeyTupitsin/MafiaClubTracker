62a2971134b7e4ea712825b3ad385ea3303f983f
AlexeyTupitsin <alexistupitsin@gmail.com>
2026-09-24 16:30:24 +0300

    feat: кнопка «Итоги сезона» на дашборде

--- src/App.jsx
+++ src/App.jsx
@@ -264,6 +264,7 @@ export default function App() {
       seasons={seasons}
       currentSeasonId={currentSeasonId}
       allGames={allGames}
+      tournaments={tournaments}
       showToast={showToast}
     />
   );
